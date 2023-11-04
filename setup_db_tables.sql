a2643bc70e74d97b465f9422796142cba9e33c1e
rmaduro <202200166@estudantes.ips.pt>
2023-11-04 23:18:12 +0000

    migrate parcial

--- setup_db_tables.sql
+++ setup_db_tables.sql
@@ -8,183 +8,321 @@ IF NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = 'AdventureW
     CREATE DATABASE AdventureWorks;
 GO
 
+USE AdventureWorks;
+
+-- Drop the schemas if they exist
+DROP SCHEMA IF EXISTS Sales
+GO
+DROP SCHEMA IF EXISTS Production
+GO
+
+-- Create the Sales schema
+CREATE SCHEMA Sales;
+GO
+
+-- Create the Production schema
+CREATE SCHEMA Production;
+GO
+
 
 -- Drop the tables if they exist
-IF OBJECT_ID('SalesTerritory_Sales', 'U') IS NOT NULL
-    DROP TABLE SalesTerritory_Sales;
-IF OBJECT_ID('Sales_Currency', 'U') IS NOT NULL
-    DROP TABLE Sales_Currency;
-IF OBJECT_ID('Description', 'U') IS NOT NULL
-    DROP TABLE Description;
-IF OBJECT_ID('Product', 'U') IS NOT NULL
-    DROP TABLE Product;
-IF OBJECT_ID('Address', 'U') IS NOT NULL
-    DROP TABLE Address;
-IF OBJECT_ID('Client', 'U') IS NOT NULL
-    DROP TABLE Client;
-IF OBJECT_ID('Sales', 'U') IS NOT NULL
-    DROP TABLE Sales;
-IF OBJECT_ID('Category', 'U') IS NOT NULL
-    DROP TABLE Category;
-IF OBJECT_ID('Currency', 'U') IS NOT NULL
-    DROP TABLE Currency;
-IF OBJECT_ID('SalesTerritory', 'U') IS NOT NULL
-    DROP TABLE SalesTerritory;
-GO				
+IF OBJECT_ID('Sales.Sales_Currency', 'U') IS NOT NULL
+    DROP TABLE Sales.Sales_Currency;
+IF OBJECT_ID('Sales.SalesTerritory_Sales', 'U') IS NOT NULL
+    DROP TABLE Sales.SalesTerritory_Sales;
+IF OBJECT_ID('Sales.Customer', 'U') IS NOT NULL
+    DROP TABLE Sales.Customer;
+IF OBJECT_ID('Sales.Address', 'U') IS NOT NULL
+    DROP TABLE Sales.Address;
+IF OBJECT_ID('Production.Product', 'U') IS NOT NULL
+    DROP TABLE Production.Product;
+IF OBJECT_ID('Sales.Sales', 'U') IS NOT NULL
+    DROP TABLE Sales.Sales;
+IF OBJECT_ID('Sales.Currency', 'U') IS NOT NULL
+    DROP TABLE Sales.Currency;
+IF OBJECT_ID('Sales.SalesTerritory', 'U') IS NOT NULL
+    DROP TABLE Sales.SalesTerritory;
+IF OBJECT_ID('Production.Description', 'U') IS NOT NULL
+    DROP TABLE Production.Description;
+IF OBJECT_ID('Production.Category', 'U') IS NOT NULL
+    DROP TABLE Production.Category;
+GO
 
 -- Category Table
-CREATE TABLE Category (
-  CategoryKey INT NOT NULL,
+CREATE TABLE Production.Category (
+  CategoryKey INT IDENTITY(1,1) PRIMARY KEY,
   FrenchCategoryName VARCHAR(50),
-  ParentCategoryKey VARCHAR(50),
+  -- SubCategoryKey TINYINT,
   EnglishCategoryName VARCHAR(50),
-  SpanishCategoryName VARCHAR(50),
-  PRIMARY KEY (CategoryKey)
+  SpanishCategoryName VARCHAR(50)
 );
 
 -- Currency Table
-CREATE TABLE Currency (
-  CurrencyKey INT NOT NULL,
-  CurrencyAlternateKey INT NOT NULL,
-  CurrencyName VARCHAR(50) NOT NULL,
-  PRIMARY KEY (CurrencyKey)
+CREATE TABLE Sales.Currency (
+  CurrencyKey TINYINT IDENTITY(1,1) PRIMARY KEY,
+  CurrencyAlternateKey VARCHAR(50),
+  CurrencyName VARCHAR(50)
 );
 
 -- SalesTerritory Table
-CREATE TABLE SalesTerritory (
-  SalesTerritoryKey INT NOT NULL,
-  SalesTerritoryCountry VARCHAR(255) NOT NULL,
-  SalesTerritoryRegion VARCHAR(255) NOT NULL,
-  SalesTerritoryGroup VARCHAR(255) NOT NULL,
-  PRIMARY KEY (SalesTerritoryKey)
-);
-
--- Sales Table
-CREATE TABLE Sales (
-  SalesOrderNumber INT NOT NULL,
-  DueDate DATE NOT NULL,
-  OrderDate DATE NOT NULL,
-  CustomerPONumber INT NOT NULL,
-  CarrierTrackingNumber INT NOT NULL,
-  TaxAmt INT NOT NULL,
-  SalesAmount INT NOT NULL,
-  DueDateKey INT NOT NULL,
-  PromotionKey INT NOT NULL,
-  SalesOrderLineNumber INT NOT NULL,
-  DiscountAmount INT NOT NULL,
-  UnitPriceDiscountPct INT NOT NULL,
-  OrderQuantity INT NOT NULL,
-  ProductStandardCost INT NOT NULL,
-  UnitPrice INT NOT NULL,
-  TotalProductCost INT NOT NULL,
-  OrderDateKey INT NOT NULL,
-  ExtendedAmount INT NOT NULL,
-  RevisionNumber INT NOT NULL,
-  ShipDateKey INT NOT NULL,
-  ShipDate INT NOT NULL,
-  CurrencyKey INT NOT NULL,
-  PRIMARY KEY (SalesOrderNumber),
-  FOREIGN KEY (CurrencyKey) REFERENCES Currency(CurrencyKey)
-);
-
--- Client Table
-CREATE TABLE Client (
-  CustomerKey INT NOT NULL,
-  LastName VARCHAR(255) NOT NULL,
-  NameStyle BIT NOT NULL,
-  BirthDate DATE NOT NULL,
-  MaritalStatus BIT NOT NULL,
-  Gender VARCHAR(50) NOT NULL,
-  EmailAddress VARCHAR(255) NOT NULL,
-  YearlyIncome INT NOT NULL,
-  Title VARCHAR(50) NOT NULL,
-  FirstNameAttribute VARCHAR(50) NOT NULL,
-  MiddleName VARCHAR(255) NOT NULL,
-  TotalChildren INT NOT NULL,
-  NumberChildrenAtHome INT NOT NULL,
-  EducationLevel INT NOT NULL CHECK (EducationLevel IN (1, 2, 3, 4, 5)), -- 1: High School, 2: College, 3: Graduate, 4: Post-graduate, 5: Other
-  Occupation VARCHAR(50) NOT NULL,
-  HouseOwnerFlag BIT NOT NULL,
-  NumberCarsOwned INT NOT NULL,
-  Phone VARCHAR(255) NOT NULL,
-  DateFirstPurchase DATE NOT NULL,
-  CommuteDistance INT NOT NULL,
-  PRIMARY KEY (CustomerKey)
-);
-
--- SalesTerritory_Sales Table
-CREATE TABLE SalesTerritory_Sales (
-  SalesTerritoryKey INT NOT NULL,
-  SalesOrderNumber INT NOT NULL,
-  FOREIGN KEY (SalesTerritoryKey) REFERENCES SalesTerritory(SalesTerritoryKey),
-  FOREIGN KEY (SalesOrderNumber) REFERENCES Sales(SalesOrderNumber),
-  PRIMARY KEY (SalesTerritoryKey, SalesOrderNumber)
-);
-
--- Sales_Currency Table
-CREATE TABLE Sales_Currency (
-  SalesOrderNumber INT NOT NULL,
-  CurrencyKey INT NOT NULL,
-  FOREIGN KEY (SalesOrderNumber) REFERENCES Sales(SalesOrderNumber),
-  FOREIGN KEY (CurrencyKey) REFERENCES Currency(CurrencyKey),
-  PRIMARY KEY (SalesOrderNumber, CurrencyKey)
-);
-
--- Product Table
-CREATE TABLE Product (
-  productKey INT NOT NULL,
-  Size INT NOT NULL,
-  SizeUnitMeasureCode INT NOT NULL,
-  DaysToManufacture INT NOT NULL,
-  Color VARCHAR(50) NOT NULL,
-  SizeRange INT NOT NULL,
-  StandardCost INT NOT NULL,
-  ListPrice INT NOT NULL,
-  SafetyStockLevel INT NOT NULL,
-  WeightUnitMeasureCode INT NOT NULL,
-  FinishedGoodsFlag BIT NOT NULL,
-  Weight INT NOT NULL,
-  Class VARCHAR(50) NOT NULL,
-  ProductLine VARCHAR(50) NOT NULL,
-  DealerPrice INT NOT NULL,
-  ModelName VARCHAR(50) NOT NULL,
-  Status VARCHAR(20) NOT NULL CHECK (Status IN ('Available', 'Out of Stock', 'Discontinued', 'In Production')), -- Enum for status
-  CategoryKey INT NOT NULL,
-  SalesOrderNumber INT NOT NULL,
-  FOREIGN KEY (CategoryKey) REFERENCES Category(CategoryKey),
-  FOREIGN KEY (SalesOrderNumber) REFERENCES Sales(SalesOrderNumber),
-  PRIMARY KEY (productKey)
+CREATE TABLE Sales.SalesTerritory (
+  SalesTerritoryKey INT IDENTITY(1,1) PRIMARY KEY,
+  SalesTerritoryCountry VARCHAR(255),
+  SalesTerritoryRegion VARCHAR(255),
+  SalesTerritoryGroup VARCHAR(255)
 );
 
 -- Address Table
-CREATE TABLE Address (
-  AddressKey INT NOT NULL,
-  StateProvince INT NOT NULL,
-  CountryRegion INT NOT NULL,
-  City VARCHAR(50) NOT NULL,
-  AddressLine2 VARCHAR(255) NOT NULL,
-  AddressLine1 VARCHAR(255) NOT NULL,
-  PostalCode Varchar(50) NOT NULL,
-  StateProvinceName VARCHAR(255) NOT NULL,
-  CountryRegionName VARCHAR(255) NOT NULL,
-  SalesTerritoryKey INT NOT NULL,
-  CustomerKey INT NOT NULL,
-  FOREIGN KEY (SalesTerritoryKey) REFERENCES SalesTerritory(SalesTerritoryKey),
-  FOREIGN KEY (CustomerKey) REFERENCES Client(CustomerKey),
-  PRIMARY KEY (AddressKey)
+CREATE TABLE Sales.Address (
+  AddressKey INT IDENTITY(1,1) PRIMARY KEY,
+  StateProvince VARCHAR(50),
+  CountryRegion VARCHAR(50),
+  City VARCHAR(50),
+  AddressLine2 VARCHAR(255),
+  AddressLine1 VARCHAR(255),
+  PostalCode Varchar(50),
+  StateProvinceName VARCHAR(255),
+  CountryRegionName VARCHAR(255),
+  SalesTerritoryKey INT,
+  FOREIGN KEY (SalesTerritoryKey) REFERENCES Sales.SalesTerritory(SalesTerritoryKey)
+);
+
+-- Sales Table
+CREATE TABLE Sales.Sales (
+  SalesKey INT IDENTITY(1,1) PRIMARY KEY,
+  SalesOrderNumber VARCHAR(50),
+  DueDate DATE,
+  OrderDate DATE,
+  CustomerPONumber SMALLINT,
+  CarrierTrackingNumber TINYINT,
+  TaxAmt SMALLINT,
+  SalesAmount FLOAT,
+  DueDateKey DATE,
+  PromotionKey TINYINT,
+  SalesOrderLineNumber TINYINT,
+  DiscountAmount TINYINT,
+  UnitPriceDiscountPct TINYINT,
+  OrderQuantity TINYINT,
+  ProductStandardCost FLOAT,
+  UnitPrice FLOAT,
+  TotalProductCost FLOAT,
+  OrderDateKey DATE,
+  ExtendedAmount FLOAT,
+  RevisionNumber TINYINT,
+  ShipDateKey DATE,
+  ShipDate DATETIME2(7),
+  CurrencyKey TINYINT,
+  FOREIGN KEY (CurrencyKey) REFERENCES Sales.Currency(CurrencyKey)
+);
+
+-- Client Table
+CREATE TABLE Sales.Customer (
+  CustomerKey INT IDENTITY(1,1) PRIMARY KEY,
+  LastName VARCHAR(255),
+  NameStyle VARCHAR(50),
+  BirthDate DATE,
+  MaritalStatus VARCHAR(50),
+  Gender VARCHAR(50),
+  EmailAddress VARCHAR(50),
+  YearlyIncome INT,
+  Title VARCHAR(10),
+  MiddleName VARCHAR(50),
+  TotalChildren TINYINT,
+  NumberChildrenAtHome TINYINT,
+  EducationLevel VARCHAR(50),
+  Occupation VARCHAR(50),
+  HouseOwnerFlag BIT,
+  NumberCarsOwned TINYINT,
+  Phone VARCHAR(50),
+  DateFirstPurchase DATE,
+  CommuteDistance VARCHAR(50),
+  AddressKey INT,
+  FOREIGN KEY (AddressKey) REFERENCES Sales.Address(AddressKey)
+);
+
+-- SalesTerritory_Sales Table
+CREATE TABLE Sales.SalesTerritory_Sales (
+  SalesTerritoryKey INT,
+  SalesKey INT,
+  FOREIGN KEY (SalesTerritoryKey) REFERENCES Sales.SalesTerritory(SalesTerritoryKey),
+  FOREIGN KEY (SalesKey) REFERENCES Sales.Sales(SalesKey),
+  PRIMARY KEY (SalesTerritoryKey, SalesKey)
+);
+
+-- Sales_Currency Table
+CREATE TABLE Sales.Sales_Currency (
+  SalesKey INT,
+  CurrencyKey TINYINT,
+  FOREIGN KEY (SalesKey) REFERENCES Sales.Sales(SalesKey),
+  FOREIGN KEY (CurrencyKey) REFERENCES Sales.Currency(CurrencyKey),
+  PRIMARY KEY (SalesKey, CurrencyKey)
+);
+
+-- Product Table
+CREATE TABLE Production.Product (
+  productKey INT IDENTITY(1,1) PRIMARY KEY,
+  Size VARCHAR(50),
+  SizeUnitMeasureCode VARCHAR(50),
+  DaysToManufacture TINYINT,
+  Color VARCHAR(50),
+  SizeRange VARCHAR(50),
+  StandardCost FLOAT,
+  ListPrice FLOAT,
+  SafetyStockLevel SMALLINT,
+  WeightUnitMeasureCode VARCHAR(50),
+  FinishedGoodsFlag VARCHAR(50),
+  Weight FLOAT,
+  Class VARCHAR(50),
+  ProductLine VARCHAR(50),
+  DealerPrice FLOAT,
+  ModelName VARCHAR(50),
+  Status VARCHAR(50),
+  CategoryKey INT,
+  SalesKey INT,
+  FOREIGN KEY (CategoryKey) REFERENCES Production.Category(CategoryKey),
+  FOREIGN KEY (SalesKey) REFERENCES Sales.Sales(SalesKey)
 );
 
 -- Description Table
-CREATE TABLE Description (
-  DescriptionKey INT NOT NULL,
-  FrenchDescription VARCHAR(255) NOT NULL,
-  EnglishDescription VARCHAR(255) NOT NULL,
-  SpanishProductName VARCHAR(255) NOT NULL,
-  EnglishProductName VARCHAR(255) NOT NULL,
-  FrenchProductName VARCHAR(255) NOT NULL,
-  SpanishDescription VARCHAR(255) NOT NULL,
-  productKey INT NOT NULL,
-  FOREIGN KEY (productKey) REFERENCES Product(productKey),
-  PRIMARY KEY (DescriptionKey)
+CREATE TABLE Production.Description (
+  DescriptionKey INT IDENTITY(1,1) PRIMARY KEY,
+  FrenchDescription NVARCHAR(1000), 
+  EnglishDescription NVARCHAR(255),
+  SpanishProductName NVARCHAR(255),
+  EnglishProductName NVARCHAR(255),
+  FrenchProductName NVARCHAR(255),
+  productKey INT,
+  FOREIGN KEY (productKey) REFERENCES Production.Product(productKey)
 );
 
+
+-- Data Migration
+
+DROP PROCEDURE IF EXISTS Sales.MigrateAddress
+GO
+DROP PROCEDURE IF EXISTS Sales.MigrateCustomer
+GO
+DROP PROCEDURE IF EXISTS Sales.MigrateCurrency
+GO
+DROP PROCEDURE IF EXISTS Sales.MigrateSales
+GO
+DROP PROCEDURE IF EXISTS Sales.MigrateSales_Currency
+GO
+DROP PROCEDURE IF EXISTS Sales.MigrateSalesTerritory
+GO
+DROP PROCEDURE IF EXISTS Sales.MigrateSalesTerritory_Sales
+GO
+DROP PROCEDURE IF EXISTS Production.MigrateCategory
+GO
+DROP PROCEDURE IF EXISTS Production.MigrateDescription
+GO
+DROP PROCEDURE IF EXISTS Production.MigrateProduct
+GO
+
+
+CREATE PROCEDURE Sales.MigrateAddress
+AS
+BEGIN
+
+    INSERT INTO Sales.Address (StateProvince, CountryRegion, City, AddressLine1, AddressLine2, PostalCode, StateProvinceName, CountryRegionName)
+    SELECT  StateProvinceCode, CountryRegionCode, City, AddressLine1, AddressLine2, PostalCode, StateProvinceName, CountryRegionName
+    FROM AdventureWorksOld7.ADOld.Customer;
+END;
+GO
+
+EXEC Sales.MigrateAddress;
+GO
+
+
+CREATE PROCEDURE Production.MigrateProduct 
+AS
+BEGIN 
+	INSERT INTO Production.Product (Size, SizeUnitMeasureCode, DaysToManufacture, Color, SizeRange, StandardCost, ListPrice, SafetyStockLevel, WeightUnitMeasureCode, FinishedGoodsFlag, Weight, Class, ProductLine, DealerPrice, ModelName, Status) 
+	SELECT Size, SizeUnitMeasureCode, DaysToManufacture, Color, SizeRange, StandardCost, ListPrice, SafetyStockLevel, WeightUnitMeasureCode, FinishedGoodsFlag, Weight, Class, ProductLine, DealerPrice, ModelName, Status 
+	FROM AdventureWorksOld7.ADOld.Products; 
+END;
+GO
+
+EXEC Production.MigrateProduct
+GO
+
+
+  CREATE PROCEDURE Sales.MigrateCurrency 
+  AS
+  BEGIN
+	INSERT INTO Sales.Currency (CurrencyAlternateKey, CurrencyName) 
+	SELECT CurrencyAlternateKey, CurrencyName FROM AdventureWorksOld7.ADOld.Currency; 
+END;
+GO
+
+EXEC Sales.MigrateCurrency; 
+GO
+
+
+CREATE PROCEDURE Sales.MigrateSales 
+AS 
+BEGIN 
+	INSERT INTO Sales.Sales (SalesOrderNumber, DueDate, OrderDate, CustomerPONumber, CarrierTrackingNumber, TaxAmt, SalesAmount, DueDateKey, PromotionKey, SalesOrderLineNumber, DiscountAmount, UnitPriceDiscountPct, OrderQuantity, ProductStandardCost, UnitPrice, TotalProductCost, OrderDateKey, ExtendedAmount, RevisionNumber, ShipDateKey, ShipDate) 
+	SELECT SalesOrderNumber, DueDate, OrderDate, CustomerPONumber, CarrierTrackingNumber, TaxAmt, SalesAmount, DueDateKey, PromotionKey, SalesOrderLineNumber, DiscountAmount, UnitPriceDiscountPct, OrderQuantity, ProductStandardCost, UnitPrice, TotalProductCost, OrderDateKey, ExtendedAmount, RevisionNumber, ShipDateKey, ShipDate
+	FROM AdventureWorksOld7.ADOld.sales7Temp; 
+END;
+GO
+
+EXEC Sales.MigrateSales; 
+GO
+
+
+CREATE PROCEDURE Sales.MigrateSalesTerritory
+AS
+BEGIN
+    INSERT INTO Sales.SalesTerritory (SalesTerritoryCountry, SalesTerritoryRegion, SalesTerritoryGroup)
+    SELECT SalesTerritoryCountry, SalesTerritoryRegion, SalesTerritoryGroup
+    FROM AdventureWorksOld7.ADOld.SalesTerritory;
+END;
+GO
+
+EXEC Sales.MigrateSalesTerritory;
+GO
+
+
+CREATE PROCEDURE Sales.MigrateCustomer
+AS
+BEGIN
+    INSERT INTO Sales.Customer(LastName, NameStyle, BirthDate, MaritalStatus, Gender, EmailAddress, YearlyIncome, Title, MiddleName, TotalChildren, NumberChildrenAtHome, EducationLevel, Occupation, HouseOwnerFlag, NumberCarsOwned, Phone, DateFirstPurchase, CommuteDistance)
+    SELECT LastName, NameStyle, BirthDate, MaritalStatus, Gender, EmailAddress, YearlyIncome, Title, MiddleName, TotalChildren, NumberChildrenAtHome, Education, Occupation, HouseOwnerFlag, NumberCarsOwned, Phone, DateFirstPurchase, CommuteDistance
+    FROM AdventureWorksOld7.ADOld.Customer;
+END;
+GO
+
+EXEC Sales.MigrateCustomer;
+GO
+
+
+
+CREATE PROCEDURE Production.MigrateCategory
+AS
+BEGIN
+    INSERT INTO Production.Category (FrenchCategoryName, EnglishCategoryName, SpanishCategoryName)
+    SELECT FrenchProductCategoryName, EnglishProductCategoryName, SpanishProductCategoryName
+    FROM AdventureWorksOld7.ADOld.Products;
+
+	-- INSERT INTO Production.Category (SubCategoryKey)
+	-- SELECT ProductSubcategoryKey
+	-- FROM AdventureWorksOld7.ADOld.ProductSubCategory
+END;
+GO
+
+EXEC Production.MigrateCategory;
+GO
+
+
+CREATE PROCEDURE Production.MigrateDescription
+AS
+BEGIN
+    INSERT INTO Production.Description (FrenchDescription, EnglishDescription, SpanishProductName, EnglishProductName, FrenchProductName)
+    SELECT FrenchDescription, EnglishDescription, SpanishProductName, EnglishProductName, FrenchProductName
+    FROM AdventureWorksOld7.ADOld.Products;
+END;
+GO
+
+EXEC Production.MigrateDescription;
